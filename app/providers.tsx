d07a573d7d5a53c3143edf1909cb4167a7b8415c
Tony <tonymeissner70@gmail.com>
2025-11-18 18:54:12 +0100

    fix: change default theme to dark in NextThemesProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -11,7 +11,7 @@ export interface ProvidersProps {
 export function Providers({ children }: ProvidersProps) {
   return (
     <SessionProvider>
-      <NextThemesProvider attribute="class" defaultTheme="light">
+      <NextThemesProvider attribute="class" defaultTheme="dark">
         <HeroUIProvider>
           {children}
         </HeroUIProvider>
