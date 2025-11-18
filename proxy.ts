d5bb3a539299ceab91e579c2fc15df191872b8bf
Tony <tonymeissner70@gmail.com>
2025-11-18 18:38:32 +0100

    fix: remove unnecessary comment from proxy function

--- proxy.ts
+++ proxy.ts
@@ -5,7 +5,6 @@ export function proxy(request: NextRequest) {
   return NextResponse.redirect(new URL('/', request.url))
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: [
     "/media/return",
